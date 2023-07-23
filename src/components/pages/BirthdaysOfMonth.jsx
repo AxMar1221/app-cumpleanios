8137bc80c71be608eec96ea808bf50c4bdd8f786
Tachi Dito <tachidito_7inc@hotmail.com>
2023-07-23 13:06:39 -0600

    birthhdayOfMont bases

--- src/components/pages/BirthdaysOfMonth.jsx
+++ src/components/pages/BirthdaysOfMonth.jsx
@@ -1,5 +1,14 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+  Button,
+  CardContent,
+  List,
+  ListItem,
+  TableCell,
+  Typography,
+} from "@mui/material";
 import { data } from "../../data";
+import { WhatsApp } from "@mui/icons-material";
+import { grey } from "@mui/material/colors";
 
 function getBirthdaysOfMonth() {
   const month = new Date();
@@ -12,9 +21,17 @@ function getBirthdaysOfMonth() {
 
 export const BirthdaysOfMonth = () => {
   const months = getBirthdaysOfMonth();
+
+  const sendWhatsMessage = (phone) => {
+    const message = "¡Feliz cumpleaños!";
+    const whatsURL = `https://api.whatsapp.com/send/?phone=+52${phone}&text=${encodeURIComponent(
+      message
+    )}`;
+    window.open(whatsURL, "_blank");
+  };
   return (
     <>
-      <div>
+      <CardContent className="birth__month">
         <Typography variant="h4" gutterBottom>
           Cumpleaños del Mes
         </Typography>
@@ -22,17 +39,38 @@ export const BirthdaysOfMonth = () => {
           <List>
             {months.map((person, i) => (
               <ListItem key={i}>
-                <ListItemText
-                  primary={person.nombre}
-                  secondary={`Feca de cumpleaños: ${person.cumpleanios}, Departamento: ${person.departamento}`}
-                />
+                <TableCell>{person.nombre}</TableCell>
+                <TableCell>{person.cumpleanios}</TableCell>
+                <TableCell align="center">
+                  {person.telefono.trim() !== "" ? (
+                    <Button
+                      color="success"
+                      size="small"
+                      variant="contained"
+                      sx={{ color: grey[50] }}
+                      startIcon={<WhatsApp />}
+                      onClick={() => sendWhatsMessage(person.telefono)}
+                    >
+                      Enviar WhatsApp
+                    </Button>
+                  ) : (
+                    <Button
+                      color="error"
+                      size="small"
+                      variant="contained"
+                      sx={{ color: grey[50] }}
+                    >
+                      N/A
+                    </Button>
+                  )}
+                </TableCell>
               </ListItem>
             ))}
           </List>
         ) : (
           <Typography>No hay cumpleaños este mes.</Typography>
         )}
-      </div>
+      </CardContent>
     </>
   );
 };
